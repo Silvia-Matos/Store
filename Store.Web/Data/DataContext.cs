b32fbcff3dc00b65e6ad6a86049627cf4ce1ffbb
Silvia Matos <Silvia.Duarte.Matos@formandos.cinel.pt>
2021-01-14 22:42:54 +0000

    Create BD and Products Controller

---
+++ Store.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Store.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Store.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Store.Web/Migrations/20210114222806_ModifyProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Store.Web.Migrations
+{
+    public partial class ModifyProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "Products");
+        }
+    }
+}
